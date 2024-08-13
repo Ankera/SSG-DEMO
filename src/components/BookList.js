3a54b85a68c2fe538033b3cf65ff3e82af700f78
ankera <15189120919@163.com>
2024-08-13 22:19:27 +0800

    feat: schema 协议

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React from "react";
+
+const jsonData = {
+  "@context": "https://schema.org",
+  "@type": "Product",
+  "name": "deals",
+  "brand": {
+    "@type": "Brand",
+    "name": "VEVOR"
+  },
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "100",
+    "ratingCount": "80"
+  }
+};
+
+const jsonLD = JSON.stringify(jsonData, null, 4);
 
 const BookList = ({ books }) => {
   return (
     <div>
+      <script type="application/ld+json" dangerouslySetInnerHTML={{__html: jsonLD}}></script>
       <h2>Book List</h2>
-      <ul className='book_ul'>
+      <ul className="book_ul">
         {(books.data || []).map((book) => (
           <li key={book.id}>
-            <h3>{book.name || 'Unknown Book'}</h3>
-            <p>{book.description || 'No description available'}</p>
-            <p>Type: {book.type || 'Unknown'}</p>
+            <h3>{book.name || "Unknown Book"}</h3>
+            <p>{book.description || "No description available"}</p>
+            <p>Type: {book.type || "Unknown"}</p>
           </li>
         ))}
       </ul>
@@ -18,4 +36,3 @@ const BookList = ({ books }) => {
 };
 
 export default BookList;
-
