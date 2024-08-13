50cd4232d0188a1f2c961001ec39ff1b29ad7364
ankera <15189120919@163.com>
2024-08-13 22:02:13 +0800

    feat: css

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './booklist.css';
 
 const BookList = ({ books }) => {
   return (
     <div>
       <h2>Book List</h2>
-      <ul className='ul'>
+      <ul className='book_ul'>
         {(books.data || []).map((book) => (
           <li key={book.id}>
             <h3>{book.name || 'Unknown Book'}</h3>
